11299e6df9c1a5d1e80ea46d9743b302fb4996e8
Greenz <73607229+GreenzQe@users.noreply.github.com>
2025-11-25 12:35:17 +0100

    token check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,33 @@
 import './App.css'
 import LoginPage from "./Components/LoginPage.tsx";
 import Dashboard from "./Components/Dashboard.tsx";
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router";
+import ProtectedRoute from "./Components/ProtectedRoute.tsx";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 
 function App() {
 
 
-  return(
-    <RouterProvider router={createBrowserRouter([
-        {
-            path: '/',
-            element: <Outlet ></Outlet >,
-            children: [
-                {
-                    path: "/",
-                    element: <LoginPage/>
-
-                },
-                {
-
-                    path: '/dashboard',
-                    element: <Dashboard/>
-                }
-            ]
-        }
-    ])}/>
+    return (
+        <RouterProvider router={createBrowserRouter([
+            {
+                path: '/',
+                element: <Outlet/>,
+                children: [
+                    {
+                        index: true,
+                        element: <LoginPage/>
+                    },
+                    {
+                        path: '/dashboard',
+                        element: (
+                            <ProtectedRoute>
+                                <Dashboard/>
+                            </ProtectedRoute>
+                        )
+                    }
+                ]
+            }
+        ])}/>
     )
 }
-
 export default App
