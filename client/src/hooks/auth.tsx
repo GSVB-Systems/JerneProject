aa4d6c361a23f0f04b828edc9cdd56cbe1e0fc67
Greenz <73607229+GreenzQe@users.noreply.github.com>
2025-12-02 13:29:01 +0100

    Create auth.tsx

---
+++ client/src/hooks/auth.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { authClient } from "../api-clients";
+import type { UserDto, LoginRequest } from "../models/ServerAPI.ts";
+import { useAtom } from "jotai";
+import { tokenAtom, userInfoAtom } from "../atoms/token";
+
+type AuthHook = {
+    user: UserDto | null;
+    login: (request: LoginRequest) => Promise<void>;
+    logout: () => void;
+};
+
+export const useAuth = () => {
+    const [_, setJwt] = useAtom(tokenAtom);
+    const [user] = useAtom(userInfoAtom);
+    const navigate = useNavigate();
+
+    const login = async (request: LoginRequest) => {
+        const response = await authClient.login(request);
+        setJwt(response.jwt!);
+        navigate("/");
+    };
+
+    const logout = async () => {
+        setJwt(null);
+        navigate("/login");
+    };
+
+    return {
+        user,
+        login,
+        logout,
+    } as AuthHook;
+};
