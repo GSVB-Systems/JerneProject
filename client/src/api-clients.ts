6666a6802cf435cb6ec2e7aa942eaa5489d42444
Greenz <73607229+GreenzQe@users.noreply.github.com>
2025-12-02 11:55:42 +0100

    jwt startup on client

---
+++ client/src/api-clients.ts
@@ -0,0 +1,24 @@
+import { TOKEN_KEY, tokenStorage } from "./atoms/token.ts";
+import {
+    AuthClient,
+} from "./models/ServerAPI.ts";
+
+const customFetch = async (url: RequestInfo, init?: RequestInit) => {
+    const token = tokenStorage.getItem(TOKEN_KEY, null);
+
+    if (token) {
+        // Copy of existing init or new object, with copy of existing headers or
+        // new object including Bearer token.
+        init = {
+            ...(init ?? {}),
+            headers: {
+                ...(init?.headers ?? {}),
+                Authorization: `Bearer ${token}`,
+            },
+        };
+    }
+    return await fetch(url, init);
+};
+
+const baseUrl = undefined;
+export const authClient = new AuthClient(baseUrl, { fetch: customFetch });
