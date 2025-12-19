7faff02833c71fd01b255e2fcc11cf05432f1818
Greenz <73607229+GreenzQe@users.noreply.github.com>
2025-12-19 10:14:18 +0100

    Update AuthService.cs

--- server/service/Services/AuthService.cs
+++ server/service/Services/AuthService.cs
@@ -89,7 +89,7 @@ public class AuthService : Service<User, User, User>, IAuthService
 
     public async Task<User> UpdateUserPasswordAsync(string userId, string oldPassword, string newPassword)
     {
-        _authRules.ValidateUpdateUserPasswordAsync(userId, oldPassword, newPassword);
+        await _authRules.ValidateUpdateUserPasswordAsync(userId, oldPassword, newPassword);
         var user = await base.GetByIdAsync(userId);
         if (user is null)
             throw new InvalidOperationException("Brugeren ikke fundet.");
